5bd46ecb1e08d62999bbc95f9c6728c3da6db58f
Divyam <agrawaldivyam2001@gmail.com>
2023-02-24 02:26:54 +0530

    Testing devnet wallet

--- web/src/hooks/userFlowUser.tsx
+++ web/src/hooks/userFlowUser.tsx
@@ -7,8 +7,12 @@ export function useFlowUser() {
   useMemo(() => {
     fcl
       .config()
+      // Point App at Emulator REST API
+      // .put("accessNode.api", "https://fcl-discovery.onflow.org/api/local/authn")
+      // Point FCL at dev-wallet (default port)
+      // .put("discovery.wallet", "https://fcl-discovery.onflow.org/local/authn")
       .put("accessNode.api", process.env.NEXT_PUBLIC_FLOW_ACCESS_API) // connect to Flow
-      .put("discovery.wallet", process.env.NEXT_PUBLIC_WALLET_API) // use Blocto wallet
+      .put("discovery.wallet", process.env.NEXT_PUBLIC_WALLET_API) // use Dapper wallet
 
       // use pop instead of default IFRAME/RPC option for security enforcement
       .put("discovery.wallet.method", "POP/RPC")
