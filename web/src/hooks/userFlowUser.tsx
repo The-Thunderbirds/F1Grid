cf05e6b07a7416fc15bbf2391adfae9c6a165c64
Divyam <agrawaldivyam2001@gmail.com>
2023-02-24 20:10:52 +0530

    Added Discovery wallets including Dapper

--- web/src/hooks/userFlowUser.tsx
+++ web/src/hooks/userFlowUser.tsx
@@ -12,7 +12,8 @@ export function useFlowUser() {
       // Point FCL at dev-wallet (default port)
       // .put("discovery.wallet", "https://fcl-discovery.onflow.org/local/authn")
       .put("accessNode.api", process.env.NEXT_PUBLIC_FLOW_ACCESS_API) // connect to Flow
-      .put("discovery.wallet", process.env.NEXT_PUBLIC_WALLET_API) // use Dapper wallet
+      .put("discovery.wallet", process.env.NEXT_PUBLIC_WALLET_API) 
+      .put("discovery.authn.include", "0x82ec283f88a62e65") // use Dapper wallet
 
       // use pop instead of default IFRAME/RPC option for security enforcement
       .put("discovery.wallet.method", "POP/RPC")
